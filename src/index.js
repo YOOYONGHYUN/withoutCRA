262ae598def848f1ede7ce514f30c432f325da93
YOOYONGHYUN <yyh901@naver.com>
2022-08-12 02:43:27 +0900

    withoutCRA
    CRA없이 react, react-dom, babel, webpack으로 리액트 구현하기

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react";
+import FollowButton from "./components/FollowBtn";
+
+const domContainer = document.querySelector("#root");
+ReactDOM.render(<FollowButton />, domContainer);
